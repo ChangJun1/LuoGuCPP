2adff94bf03ab9f1436f0b54500b93f5265cadc8
aichangjun <190589618@qq.com>
2023-07-24 21:51:06 +0800

    answer for B3648

---
+++ B3648.cpp
@@ -0,0 +1,11 @@
+// https://www.luogu.com.cn/problem/B3648
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    cout << "I am " << x << " years old.";
+    return 0;
+}
