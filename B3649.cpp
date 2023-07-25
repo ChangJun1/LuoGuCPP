f0db169cd748c7c78560cd8365d24ed061d47d56
aichangjun <190589618@qq.com>
2023-07-25 23:32:49 +0800

    answer for B3649

---
+++ B3649.cpp
@@ -0,0 +1,15 @@
+// https://www.luogu.com.cn/problem/B3649
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if (a <= b) {
+        cout << "YE5" << endl;
+    } else {
+        cout << "N0" << endl;
+    }
+    return 0;
+}
